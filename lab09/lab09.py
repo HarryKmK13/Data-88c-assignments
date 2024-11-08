9f8abd482a3e5d5e44825b8d10b4dbc43712ae1b
HarryKmK13 <harryhuang13@icloud.com>
2024-11-08 14:16:18 -0800

    Add files via upload

---
+++ lab09/lab09.py
@@ -0,0 +1,91 @@
+def max_path_sum(t):
+    """Return the maximum path sum of the tree.
+
+    >>> t = Tree(1, [Tree(5, [Tree(1), Tree(3)]), Tree(10)])
+    >>> max_path_sum(t)
+    11
+    """
+    if not t.branches:  # Base case: if the tree has no branches, it's a leaf
+        return t.label
+    # Recursive case: get the maximum path sum of all branches
+    branch_sums = [max_path_sum(branch) for branch in t.branches]
+    return t.label + max(branch_sums)
+
+
+def find_path(t, x):
+    """
+    >>> t = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # returns None
+    """
+    if t.label == x:
+        return [t.label]
+    for b in t.branches:
+        path = find_path(b,x)
+        if path:
+            return [t.label] + path
+    return None
+
+def prune_small(t, n):
+    """Prune the tree mutatively, keeping only the n branches
+    of each node with the smallest labels.
+
+    >>> t1 = Tree(6)
+    >>> prune_small(t1, 2)
+    >>> t1
+    Tree(6)
+    >>> t2 = Tree(6, [Tree(3), Tree(4)])
+    >>> prune_small(t2, 1)
+    >>> t2
+    Tree(6, [Tree(3)])
+    >>> t3 = Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2), Tree(3)]), Tree(5, [Tree(3), Tree(4)])])
+    >>> prune_small(t3, 2)
+    >>> t3
+    Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
+    """
+    while len(t.branches) > n:
+        largest = max(t.branches, key= lambda b: b.label)
+        t.branches.remove(largest)
+    for b in t.branches:
+        prune_small(b,n)
+
+
+
+class Tree:
+    """A tree has a label and a list of branches.
+
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        self.label = label
+        for branch in branches:
+            assert isinstance(branch, Tree)
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(repr(self.label), branch_str)
+
+    def __str__(self):
+        return '\n'.join(self.indented())
+
+    def indented(self):
+        lines = []
+        for b in self.branches:
+            for line in b.indented():
+                lines.append('  ' + line)
+        return [str(self.label)] + lines
+
